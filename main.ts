b64c11e3f0013bbc21c40daa09fb8b40ab930dc1
Tornith <jeerdus55@gmail.com>
2023-06-05 09:33:45 +0200

    Main plugin functionality

--- main.ts
+++ main.ts
@@ -1,81 +1,94 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface AsideTableSettings {
+	headerColor: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface Thumbnail {
+	url: string;
+	description?: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface TableEntry {
+	key: string;
+	value: string | string[];
+}
+
+interface Group {
+	name: string;
+	entries: TableEntry[];
+}
+
+interface AsyncTable {
+	thumbnails?: Thumbnail[];
+	groups: Group[];
+}
+
+const DEFAULT_SETTINGS: AsideTableSettings = {
+	headerColor: '#1000ff'
+}
+
+export default class AsideTable extends Plugin {
+	settings: AsideTableSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Markdown Code Block Processor
+		this.registerMarkdownCodeBlockProcessor("aside", (source, el, ctx) => {
+			const table = this.formatTable(source);
+			const container = el.createSpan({cls: "aside-table-container", });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			// Draw the thumbnails
+			const thumbnails = container.createDiv({cls: "aside-table-thumbnails"});
+			for (const thumbnail of table.thumbnails ?? []) {
+				const thumbnailDiv = thumbnails.createDiv({cls: "aside-table-thumbnail"});
+				const thumbnailImage = thumbnailDiv.createEl("img", {attr: {src: thumbnail.url}});
+				if (thumbnail.description) thumbnailImage.title = thumbnail.description;
+			}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			// Draw the groups
+			for (const group of table.groups) {
+				const groupHeader = container.createDiv({
+					cls: "aside-table-group-header",
+					text: group.name,
+					attr: {
+						style: `background-color: ${this.settings.headerColor}`
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				});
+				for (const entry of group.entries){
+					const entryDiv = container.createDiv({cls: "aside-table-entry"});
+					const entryKey = entryDiv.createEl("strong", {cls: "aside-table-entry-key", text: entry.key});
+					if (typeof entry.value === "string") {
+						const isLink = entry.value.match(/^\[\[(.*?)(?:(?<!\\)\|(.*?))?]]$/);
+						if (isLink) {
+							const note = this.getNoteLink(isLink[1]);
+							const link = entryDiv.createEl("a", {
+								cls: "internal-link aside-table-entry-value",
+								text: isLink[2] ?? note,
+								attr: {
+									href: note,
+									"data-href": note,
+									target: "_blank",
+									rel: "noopener"
+								}
+							});
+							continue;
+						}
+						const entryValue = entryDiv.createEl("span", {cls: "aside-table-entry-value", text: entry.value});
+					} else {
+						const entryValue = entryDiv.createEl("ul", {cls: "aside-table-entry-value aside-table-entry-list"});
+						for (const value of entry.value) {
+							entryValue.createEl("li", {text: value});
+						}
+					}
 				}
 			}
-		});
+		}, 100);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -89,28 +102,63 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	formatTable(source: string): AsyncTable {
+		/**
+		 * This function takes the source code and prepares it for the table.
+		 * The syntax is as follows:
+		 * At the very top, there can be one or more thumbnails, each starting on a new line with a `!` and a URL.
+		 * 	After the URL, there can be a description, which is separated from the URL by a `|`.
+		 * 	Example: `!https://example.com/image.png|This is an example image.`
+		 * Each group starts with a header, which is a line starting with a `#` and a name.
+		 * 	Example: `#Group Name`
+		 * After the header, there can be one or more entries, each starting on a new line with a `-`, key name, `:` and the value.
+		 * 	Example: `-Key:Value`
+		 * The value can be a single string or a list of strings, which are separated by a `;`.
+		 * 	Example: `-Key:Value 1;Value 2;Value 3`
+		 * The value can also be a link, which is a string starting with `[[` and ending with `]]`.
+		 * 	Example: `-Key:[[Link]]`
+		 * The value can also be a link with a description, which is a string starting with `[[` and ending with `]]` and a description, which is separated from the link by a `|`.
+		 * 	Example: `-Key:[[Link|Link Description]]`
+		 * All the special characters can be escaped with a `\`.
+		 * 	Example: `-Key:This is a \;`
+		 */
+		// Retrieve the thumbnails
+		const thumbnails = [...source.matchAll(/^!(.*?)(?:(?<!\\)\|(.*))?$/gm)].map(match => {
+			// Return the thumbnail
+			return {url: this.getImageLink(match[1]), description: match[2]};
+		});
+		// Use regex to split the source into groups
+		const groups = [...source.matchAll(/^#(.*)\n-((?:[^#]|\n)*)(?=#|$)/gm)].map(groupMatch => {
+			const entries = [...groupMatch[2].matchAll(/^-(.*?)(?<!\\):(.*)$/gm)].map(entryMatch => {
+				const key = entryMatch[1].replace("\\:", ":");
+				const value = entryMatch[2].split(/(?<!\\);/g).map(value => value.replace("\\;", ";"));
+				// Return the entry
+				return {key, value: value.length === 1 ? value[0] : value};
+			});
+			// Return the group
+			return {name: groupMatch[1], entries} as Group;
+		});
+		// Return the thumbnails and groups
+		return {thumbnails, groups};
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getNoteLink(noteName: string): string {
+		const note = this.app.metadataCache.getFirstLinkpathDest(noteName, "");
+		return note ? this.app.metadataCache.fileToLinktext(note, "") : noteName;
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	getImageLink(imageName: string): string {
+		const image = this.app.metadataCache.getFirstLinkpathDest(imageName, "");
+		// @ts-ignore
+		return image ? `app://local/${image?.vault.adapter.basePath}/${image?.path}` : imageName;
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: AsideTable;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AsideTable) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +168,17 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Aside Table Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Header color')
+			.setDesc('Sets the color of the headers in the aside table.')
+			.addColorPicker(color => color
+				.setValue(this.plugin.settings.headerColor)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.headerColor = value;
 					await this.plugin.saveSettings();
-				}));
+				})
+			)
 	}
 }
